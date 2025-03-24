eda129d621ab6ca9e120b6d2ff0ed0269b56a144
Tobiasz <schettinitobia@gmail.com>
2025-03-24 17:14:44 +0100

    translation done compl

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,10 @@
+'use client';
 import Link from "next/link";
+import { useTranslation } from "react-i18next";
 
 export default function Footer() {
+  const { t } = useTranslation("common");
+
   return (
     <footer className="w-full bg-gray-100 bg-gradient-to-t from-black via-gray-800 to-transparent text-white py-8 text-center">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center text-[10px] font-glow">
@@ -8,7 +12,8 @@ export default function Footer() {
         {/* Sinistra: Copyright */}
         <div className="md:w-1/3 p-2 flex justify-center items-center text-center">
           <p>
-            &copy; {new Date().getFullYear()} Spazio... Ai Ciompi <br/> Tutti i diritti riservati.
+            &copy; {new Date().getFullYear()} Spazio... Ai Ciompi <br />
+            {t("footer.rightsReserved")}
           </p>
         </div>
 
@@ -22,10 +27,9 @@ export default function Footer() {
         {/* Destra: Crediti */}
         <div className="md:w-1/3 p-2 flex justify-center items-center text-center">
           <p>
-            Code: T. Schettini <br/> Contents: T.N.T Media & Communication
+            Code: T. Schettini <br /> Contents: T.N.T Media & Communication
           </p>
         </div>
-
       </div>
     </footer>
   );
