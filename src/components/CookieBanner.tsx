2a51d139131989794f4c03221d0039ed4254d529
Tobiasz <schettinitobia@gmail.com>
2025-03-24 13:25:48 +0100

    added part_translation

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -27,7 +27,7 @@ export default function CookieBanner() {
   return (
     <div className="fixed bottom-0 pt-24 left-0 right-0 bg-gradient-to-t from-black via-gray-900 to-transparent transition-transform duration-300 text-white p-4 flex flex-col md:flex-row items-center justify-between z-50">
       <p className="text-[12px] text-center max-w-md">
-        Questo sito utilizza cookie per migliorare l'esperienza utente.
+        Questo sito utilizza cookie per migliorare l esperienza utente.
       </p>
       <div className="mt-2 md:mt-0 flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-2">
         <button
