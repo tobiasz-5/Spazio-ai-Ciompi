5450d0f1402bc64e09242ffcc4d37e762a87da40
Tobiasz <schettinitobia@gmail.com>
2025-03-27 11:41:59 +0100

    opt. visibil.

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -32,13 +32,13 @@ export default function CookieBanner() {
       <div className="mt-2 md:mt-0 flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-2">
         <button
           onClick={handleAccept}
-          className="bg-[#2A7FAA] hover:bg-gray-800 text-white px-2 py-1 rounded-md text-[14px]"
+          className="bg-gray-700 hover:bg-gray-900 text-white px-2 py-1 rounded-md text-[10px]"
         >
           Accetta
         </button>
         <button
           onClick={handleReject}
-          className="bg-gray-700 hover:bg-gray-800 text-white px-2 py-1 rounded-md text-[14px]"
+          className="bg-gray-800 hover:bg-gray-900 text-white px-2 py-1 rounded-md text-[10px]"
         >
           Rifiuta
         </button>
